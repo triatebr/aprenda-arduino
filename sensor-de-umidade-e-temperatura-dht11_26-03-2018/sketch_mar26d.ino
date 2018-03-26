1ffeb0c54012b5630b6cb2cf442d7ca791cdc2a0
LUBT <triate@gmail.com>
2018-03-26 09:27:51 -0300

    Add files via upload

---
+++ sensor-de-umidade-e-temperatura-dht11_26-03-2018/sketch_mar26d.ino
@@ -0,0 +1,28 @@
+//Programa : Sensor de umidade e temperatura DHT11
+//Autor : Arduino e Cia
+
+#include <dht.h>
+
+#define dht_dpin A1 //Pino DATA do Sensor ligado na porta Analogica A1
+
+dht DHT; //Inicializa o sensor
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);//Aguarda 1 seg antes de acessar as informações do sensor
+}
+
+void loop()
+{
+  DHT.read11(dht_dpin); //Lê as informações do sensor
+  Serial.print("Umidade = ");
+  Serial.print(DHT.humidity);
+  Serial.print(" %  ");
+  Serial.print("Temperatura = ");
+  Serial.print(DHT.temperature); 
+  Serial.println(" Celsius  ");
+
+  //Não diminuir o valor abaixo. O ideal é a leitura a cada 2 segundos
+  delay(2000);  
+}
