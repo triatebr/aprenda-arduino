08016667663349a05d222ced75a3634973f79c0b
Lucas <triate@gmail.com>
2018-05-15 16:34:38 -0400

    Add files via upload

---
+++ nodemcu-primeiros-passos-comecando-blinkar_15-05-2018/nodemcu-primeiros-passos-comecando-blinkar_15-05-2018.ino
@@ -0,0 +1,22 @@
+int ledState = LOW;     
+const int ledPin = 13;
+
+unsigned long previousMillis = 0;
+const long interval = 4000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;   
+    if (ledState == LOW)
+      ledState = HIGH;  
+    else
+      ledState = LOW;  
+    digitalWrite(ledPin, ledState);
+  }
+}
