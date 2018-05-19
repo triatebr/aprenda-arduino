0e7ede9bd8a74eb9bc6c3a5c442c83c6b911cd9d
Lucas <triate@gmail.com>
2018-05-19 15:31:34 -0300

    Add files via upload
    Curso Code IOT
    Oceam Samsung - POLI USP

---
+++ Arduino -CodeIOT 19-05-2015/sketch_may19a.ino
@@ -0,0 +1,19 @@
+// Programa realizado no CodeIOT
+// data: 19-05-2018
+// Devs: Kelvia, Mateus & Lucas
+// Objetivo: acender o LED no Arduino
+int tempo=1000;
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (led,0);
+delay (tempo);
+digitalWrite (led,1);
+delay (tempo);
+}
