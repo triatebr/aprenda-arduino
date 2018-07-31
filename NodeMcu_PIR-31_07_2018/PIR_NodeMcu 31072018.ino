9766f96ea1c9ab8ed1e44fca73f09e7d20b90fc6
Lucas <triate@gmail.com>
2018-07-31 17:05:07 -0300

    Add files via upload

---
+++ NodeMcu_PIR-31_07_2018/PIR_NodeMcu 31072018.ino
@@ -0,0 +1,21 @@
+
+int Status = 12;
+int sensor = 13;
+
+void setup() {
+  pinMode(sensor, INPUT); // declare sensor as input
+  pinMode(Status, OUTPUT);  // declare LED as output
+}
+
+void loop(){
+  long state = digitalRead(sensor);
+  delay(1000);
+    if(state == HIGH){
+      digitalWrite (Status, HIGH);
+      Serial.println("Motion detected!");
+    }
+    else {
+      digitalWrite (Status, LOW);
+      Serial.println("Motion absent!");
+      }
+}
